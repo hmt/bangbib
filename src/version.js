ec447c69a724ff68769f4684ceee364df2929deb
hmt <dev@hmt.im>
2020-02-15 08:55:13 +0100

    update deps, entferne unbenutzte Dateien

--- src/version.js
+++
@@ -1,2 +0,0 @@
-// in appveyor.yml verwendete env vars
-export const VERSION = { buildID: '%APPVEYOR_BUILD_ID%', buildVersion: '%APPVEYOR_BUILD_VERSION%', buildNo: '%APPVEYOR_BUILD_NUMBER%', gitHash: '%APPVEYOR_REPO_COMMIT%', gitMessage: '%APPVEYOR_REPO_COMMIT_MESSAGE%', production: false }
