a2b0e628a520c0e21a617514b8f32e28eccce936
hmt <dev@hmt.im>
2020-08-31 11:24:26 +0200

    formatiert

--- src/components/Print/Kurs.svelte
+++ src/components/Print/Kurs.svelte
@@ -1,15 +1,15 @@
 <script>
-  import { ipcRenderer } from 'electron'
-  import { onMount } from 'svelte';
+  import { ipcRenderer } from "electron";
+  import { onMount } from "svelte";
   import { schueler, medien, titel, configData } from "./../../stores.js";
   import { chunk } from "./../../helpers.js";
 
-  const runner = _ => {
-    const d = new Date().getTime()
-    const pdf_name = `${$schueler[0].klasse}_${$schueler[0].kurs}_${$schueler[0].jahr}_${d}.pdf`
-    $configData.gruppe_print_dialog && ipcRenderer.send('print')
-    $configData.gruppe_auto_pdf && ipcRenderer.send('pdf', pdf_name)
-  }
+  const runner = (_) => {
+    const d = new Date().getTime();
+    const pdf_name = `${$schueler[0].klasse}_${$schueler[0].kurs}_${$schueler[0].jahr}_${d}.pdf`;
+    $configData.gruppe_print_dialog && ipcRenderer.send("print");
+    $configData.gruppe_auto_pdf && ipcRenderer.send("pdf", pdf_name);
+  };
 
   onMount(() => {
     setTimeout(runner, 500);
@@ -26,7 +26,7 @@
         – {new Date().toLocaleDateString('de', {
           day: '2-digit',
           month: '2-digit',
-          year: 'numeric'
+          year: 'numeric',
         })}
       </h2>
       <h2 class="subtitle">{$titel || ''}</h2>
@@ -46,7 +46,7 @@
               <td>{i * anzahl + ii + 1}</td>
               <td>{s.name}</td>
               <td>{s.vorname}</td>
-              <td>{s.gesperrt ? '– gesperrt –':''}{$medien[s.id] || ''}</td>
+              <td>{s.gesperrt ? '– gesperrt –' : ''}{$medien[s.id] || ''}</td>
               <td />
             </tr>
           {/each}
@@ -54,12 +54,12 @@
       </table>
     </div>
     <div class="grid-footer">
-        {$schueler[0].klasse} – {$schueler[0].kurs} – {$schueler[0].kurs_lehrer}
-        – {new Date().toLocaleDateString('de', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric'
-        })}
+      {$schueler[0].klasse} – {$schueler[0].kurs} – {$schueler[0].kurs_lehrer} –
+      {new Date().toLocaleDateString('de', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+      })}
     </div>
   </div>
 {/each}
