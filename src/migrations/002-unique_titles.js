5c85e4a2861fe384afd34e45cfa55686e125b800
hmt <dev@hmt.im>
2020-09-06 23:50:23 +0200

    Fix für Migration bei Titelduplikaten

--- src/migrations/002-unique_titles.js
+++ src/migrations/002-unique_titles.js
@@ -1,4 +1,13 @@
 exports.up = (DB) => {
+  DB.prepare(`
+  WITH m AS (
+    SELECT ROW_NUMBER () OVER (PARTITION BY name ORDER BY id) AS RowNum, id
+    FROM medienbezeichnung
+  )
+  UPDATE medienbezeichnung
+  SET name = name || ' (Exemplar ' || (SELECT m.RowNum FROM m WHERE m.id = medienbezeichnung.id) || ')'
+  WHERE (SELECT m.RowNum FROM m WHERE m.id = medienbezeichnung.id) > 1;
+  `).run()
   DB.prepare(
     `CREATE UNIQUE INDEX idx_medienbezeichnung_name ON medienbezeichnung (name);`
   ).run();
